a985336c7bd47ef5657e7366c0d4e504f343b225
kalmtalyst <kalmtalyst@gmail.com>
2022-04-18 21:34:05 +0900

    feat: setting routes for main and detail pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import Navigation from "./components/Navigation";
 import Carousels from "./components/Carousels";
 import Items from "./components/Items";
 import shoesData from "./data";
+import { Route, Switch } from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   let [shoes, setShoes] = useState(shoesData);
   return (
     <div className="App">
       <Navigation />
-      <Carousels />
-      <div className="container">
-        <div className="row">
-          {shoes.map((shoe, i) => {
-            return <Items shoes={shoes[i]} i={i} key={i} />;
-          })}
-        </div>
-      </div>
+      <Switch>
+        <Route exact path={"/"}>
+          <Carousels />
+          <div className="container">
+            <div className="row">
+              {shoes.map((shoe, i) => {
+                return <Items shoes={shoes[i]} i={i} key={i} />;
+              })}
+            </div>
+          </div>
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function Navigation(props) {
   return (
     <>
       <Navbar bg="dark" variant="dark" className="nav">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand>ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/detail">
+              Detail
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
