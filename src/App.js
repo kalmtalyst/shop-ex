06903ac0746ac57dc70cb0ee04541387832ba1dc
kalmtalyst <kalmtalyst@gmail.com>
2022-04-18 18:39:50 +0900

    feat: rendering shoes data with map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
+import React, { useState } from "react";
 import Navigation from "./components/Navigation";
 import Carousels from "./components/Carousels";
 import Items from "./components/Items";
+import shoesData from "./data";
 
 function App() {
+  let [shoes, setShoes] = useState(shoesData);
   return (
     <div className="App">
       <Navigation />
       <Carousels />
-      <Items />
+      <Items shoes={shoes} />
     </div>
   );
 }
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,43 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from "react-bootstrap";
 
-function Items(props) {
+function Items({ shoes }) {
   return (
     <div>
       <Row>
-        <div className="col-md-4">
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-            alt=""
-          />
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </div>
-        <div className="col-md-4">
-          <img
-            src="https://codingapple1.github.io/shop/shoes2.jpg"
-            width="100%"
-            alt=""
-          />
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </div>
-        <div className="col-md-4">
-          <img
-            src="https://codingapple1.github.io/shop/shoes3.jpg"
-            width="100%"
-            alt=""
-          />
-          <h4>상품명</h4>
-          <p>상품정보</p>
-        </div>
+        {shoes.map((shoe, i) => {
+          return (
+            <div className="col-md-4">
+              <img src={`${shoes[i].img}`} width="100%" alt="" />
+              <h4>{shoes[i].title}</h4>
+              <p>{shoes[i].content}</p>
+              <p>{shoes[i].price}</p>
+            </div>
+          );
+        })}
       </Row>
-      {/* <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row> */}
     </div>
   );
 }
