0d35f5bc8419df74c4604124ee8503f72f29ad6b
kalmtalyst <kalmtalyst@gmail.com>
2022-04-26 23:15:13 +0900

    feat: use useEffect hook on detail page and add detail scss file

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
+import "./detail.scss";
 
 let Box = styled.div`
   padding: 20px;
@@ -13,6 +14,7 @@ let Title = styled.h4`
 
 let DetailInfo = styled.div`
   width: 300px;
+  margin: 0 auto;
   h4 {
     border-top: 1px solid black;
     color: #443737;
@@ -32,23 +34,49 @@ let Button = styled.button`
 `;
 
 function Detail(props) {
+  let [alert, setAlert] = useState(true);
+  let [inputData, setInputData] = useState("");
+
+  useEffect(() => {
+    // mount시, update시 실행
+    console.log("this component has been updated");
+
+    let timer = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]); // alert라는 state가 변경 될때만 실행
   let { id } = useParams();
   let history = useHistory();
 
   let clickedShoes = props.shoes.find(function (shoe) {
     return shoe.id == id;
   });
+
+  let picNum = ++id;
+
   return (
     <div>
       <div className="container">
         <Box>
-          <Title color={"#7D0633"}>Detail Page</Title>
+          <Title className="titleColor">Detail Page</Title>
         </Box>
+        {alert === true ? (
+          <div className="my-alert my-alert-red">
+            <p>재고가 얼마 남지 않았습니다!</p>
+          </div>
+        ) : null}
+
         <div className="row">
           <div className="col-md-6">
             <img
               src={
-                `https://codingapple1.github.io/shop/shoes` + `${id}` + `.jpg`
+                `https://codingapple1.github.io/shop/shoes` +
+                `${picNum}` +
+                `.jpg`
               }
               width="100%"
               alt=""
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-// import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+  &:hover {
+    color: tomato;
+  }
+  color: teal;
+  h4 {
+    margin-top: 20px;
+  }
+`;
 function Items({ shoes, i }) {
+  let num = i + 1;
   return (
     <div className="col-md-4">
-      <img
-        src={`https://codingapple1.github.io/shop/shoes` + (i + 1) + `.jpg`}
-        width="100%"
-        alt=""
-      />
-      <h4>{shoes.title}</h4>
-      <p>{shoes.content}</p>
-      <p>{shoes.price}</p>
+      <StyledLink to={"/detail/" + i} className="item-link">
+        <img
+          src={`https://codingapple1.github.io/shop/shoes` + num + `.jpg`}
+          width="100%"
+          alt=""
+        />
+        <h4>{shoes.title}</h4>
+        <p>{shoes.content}</p>
+        <p>{shoes.price}</p>
+      </StyledLink>
     </div>
   );
 }
